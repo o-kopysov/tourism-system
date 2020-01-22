be1e5e7274cb59dc5a1ccf090ed072f6c98bf007
Julia Kopysova <julykopysova97@gmail.com>
2020-01-22 18:53:37 +0200

    Static

---
+++ static/js/more.js
@@ -0,0 +1,71 @@
+function readMore(){
+var dots = document.getElementById("dots");
+var more = document.getElementById("more");
+var bt = document.getElementById("bt");
+
+if(dots.style.display==="none")
+{dots.style.display="inline";
+bt.innerHTML="More";
+more.style.display="none";
+}
+
+else{
+dots.style.display="none";
+bt.innerHTML="Hide";
+more.style.display="inline";
+}
+}
+
+function readMore1(){
+var dots1 = document.getElementById("dots1");
+var more1 = document.getElementById("more1");
+var bt1 = document.getElementById("bt1");
+
+if(dots1.style.display==="none")
+{dots1.style.display="inline";
+bt1.innerHTML="More";
+more1.style.display="none";
+}
+
+else{
+dots1.style.display="none";
+bt1.innerHTML="Hide";
+more1.style.display="inline";
+}
+}
+
+function readMore2(){
+var dots2 = document.getElementById("dots2");
+var more2 = document.getElementById("more2");
+var bt2 = document.getElementById("bt2");
+
+if(dots2.style.display==="none")
+{dots2.style.display="inline";
+bt2.innerHTML="More";
+more2.style.display="none";
+}
+
+else{
+dots2.style.display="none";
+bt2.innerHTML="Hide";
+more2.style.display="inline";
+}
+}
+
+function readMore3(){
+var dots3 = document.getElementById("dots3");
+var more3 = document.getElementById("more3");
+var bt3 = document.getElementById("bt3");
+
+if(dots3.style.display==="none")
+{dots3.style.display="inline";
+bt3.innerHTML="More";
+more3.style.display="none";
+}
+
+else{
+dots3.style.display="none";
+bt3.innerHTML="Hide";
+more3.style.display="inline";
+}
+}
---
+++ static/js/open.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+$(".btn-slide").click(function(){
+$(".panel").slideToggle("slow");
+$(this).toggleClass("active");return false;
+});
+});
+
+$(document).ready(function(){
+$(".Savings").click(function(){
+$(".panel1").slideToggle("slow");
+$(this).toggleClass("active");return false;
+});
+});
+
+$(document).ready(function(){
+$(".Attractions").click(function(){
+$(".panel2").slideToggle("slow");
+$(this).toggleClass("active");return false;
+});
+});
+
+$(document).ready(function(){
+$(".Purchase").click(function(){
+$(".panel5").slideToggle("slow");
+$(this).toggleClass("active");return false;
+});
+});
+
+$(document).ready(function(){
+$(".Transport").click(function(){
+$(".panel3").slideToggle("slow");
+$(this).toggleClass("active");return false;
+});
+});
+
+$(document).ready(function(){
+$(".Validity").click(function(){
+$(".panel4").slideToggle("slow");
+$(this).toggleClass("active");return false;
+});
+});
+
+$(document).ready(function(){
+$(".Contact").click(function(){
+$(".panel6").slideToggle("slow");
+$(this).toggleClass("active");return false;
+});
+});
